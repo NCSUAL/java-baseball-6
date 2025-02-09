c7ccd0719f3bbd7d26d6b14606a90dbd83cae162
root <root@ip-172-26-9-61.ap-northeast-2.compute.internal>
2025-02-09 05:11:46 +0000

    "feat: model 구현"
    This reverts commit 1dafc4b6f698428f0b556826653a1e97e2014ed5.

--- src/main/java/baseball/valid/InputNumberValidator.java
+++ src/main/java/baseball/valid/InputNumberValidator.java
@@ -12,16 +12,9 @@ public class InputNumberValidator {
 	
 	public String numberValidator(){
 		formattingValid();
-		numberLengthValid();
-		numberValueValid();
-		numberDuplicationValid();
-		return this.number;
-	}
-	
-	public String resultNumberValidator(){
-		formattingValid();
-		resultNumberLengthValid();
-		resultNumberValueValid();
+		lengthValid();
+		valueValid();
+		duplicationValid();
 		return this.number;
 	}
 	
@@ -31,50 +24,35 @@ public class InputNumberValidator {
 		}
 		catch (NumberFormatException ex) {
 			// TODO: handle exception
-			throw new IllegalArgumentException(Log.NUMBERINPUTERROR.toString());
+			throw new IllegalArgumentException(Log.NUMBERINPUTERROR.getMessage());
 		}
 	}
 	
-	private void resultNumberLengthValid() {
-		if(this.number.length() != 1) {
-			throw new IllegalArgumentException(Log.RESULTNUMBERINPUTERROR.toString());
-		}
-	}
-	
-	private void resultNumberValueValid() {
-		char c = this.number.charAt(0);
-		if(c<49 || c> 57) {
-			throw new IllegalArgumentException(Log.RESULTNUMBERINPUTERROR.toString());
-		}
-	}
-	
-	
-	private void numberLengthValid() {
+	private void lengthValid() {
 		if(this.number.length() != 3) {
-			throw new IllegalArgumentException(Log.NUMBERVALIDERROR.toString());
+			throw new IllegalArgumentException(Log.NUMBERVALIDERROR.getMessage());
 		}
 	}
 	
-	private void numberValueValid() {
-		for(int a=0; a<this.number.length(); a++) {
-			char c = this.number.charAt(a);
-			if(c<49 || c>57) {
-				throw new IllegalArgumentException(Log.NUMBERINPUTERROR.toString());
-			}
+	private void valueValid() {
+		if(this.number.contains("0")) {
+			throw new IllegalArgumentException(Log.NUMBERINPUTERROR.getMessage());
 		}
 	}
 	
-	private void numberDuplicationValid() {
-		final boolean[] array = new boolean[10];
+	private void duplicationValid() {
+		final int[] array = new int[10];
 		int number= Integer.parseInt(this.number);
 		
 		while(number>0) {
-			final int index = number%10;
-			if(array[index]) {
-				throw new IllegalArgumentException(Log.NUMBERVALIDERROR.toString());
-			}
-			array[index] = true;
+			array[number%10] +=1;
 			number/=10;
+		}
+		
+		for(int arg: array) {
+			if(arg>1) {
+				throw new IllegalArgumentException(Log.NUMBERVALIDERROR.getMessage());
+			}
 		}
 	}
 }
